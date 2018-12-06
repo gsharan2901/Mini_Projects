287a712b9570673ee951c2c7bf4a371d48010043
Sharan Kumar <gsharan2901@gmail.com>
2018-12-06 16:04:24 +0530

    Halfway through

--- Text_edit.py
+++ Text_edit.py
@@ -1,6 +1,29 @@
 import tkinter as tk
 from tkinter import filedialog as fd
 w = tk.Tk()
+def new_file():
+    global txt
+    txt.delete('1.0', tk.END)
+def open_file():
+    global txt
+    openlocation1 = fd.askopenfilename(filetypes = (("text files", "*.txt"), ("All files", ".*")))
+    f1 = open(openlocation1, 'r')
+    content = f1.read()
+    txt.insert(tk.END, content)
+menu = tk.Menu(w)
+w.config(menu=menu)
+file_menu = tk.Menu(menu)
+menu.add_cascade(label = 'File', menu = file_menu)
+file_menu.add_command(label = 'New', command = new_file)
+file_menu.add_command(label = 'Open', command = open_file)
+file_menu.add_separator()
+file_menu.add_command(label = 'Exit', command = exit)
+help_menu = tk.Menu(menu)
+menu.add_cascade(label = 'Help', menu = help_menu)
+help_menu.add_command(label = 'Documentation')
+help_menu.add_command(label = 'Check for Updates')
+help_menu.add_separator()
+help_menu.add_command(label = 'About me')
 w.title("Text Editor")
 txt = tk.Text(w)
 txt.pack()
@@ -13,4 +36,6 @@ def saveas():
     file.close()
 b = tk.Button(w, text = 'Save As...', command = saveas)
 b.pack()
+q = tk.Button(w, text = 'QUIT!', activeforeground = 'red', command = quit)
+q.pack()
 w.mainloop()
