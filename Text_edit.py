49f5a67b7f0f52f3834b96842cf07469407fc8bf
Sharan Kumar <gsharan2901@gmail.com>
2018-12-06 14:23:18 +0530

    Initial Build

---
+++ Text_edit.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import filedialog as fd
+w = tk.Tk()
+w.title("Text Editor")
+txt = tk.Text(w)
+txt.pack()
+def saveas():
+    global txt
+    content = txt.get("1.0", "end-1c")
+    savelocation = fd.asksaveasfilename()
+    file = open("simple_text", "w+")
+    file.write(content)
+    file.close()    
+b = tk.Button(w, text = 'Save As...', command = saveas)
+b.pack()
+w.mainloop()
